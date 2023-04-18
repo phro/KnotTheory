c5d419262c4252cb82d2d527d25be0b632b3365d
Jesse Frohlich <jesse@frohli.ch>
2023-04-18 13:46:52 -0400

    Remove `thinPositionTests`.

--- test/Test.hs
+++ test/Test.hs
@@ -35,7 +35,6 @@ tests = TestList [ helperFunctionTests
                  , knotObjectConversionHelperTests
                  , knotObjectConversionTests
                  , namedKnotsTests
-                 , thinPositionTests
                  , tangleTests
                  ]
 
